5352a2a5a524ea976d856a8d00b9c9c64e47ec3a
Marina Monalisa do Nascimento Bagio <marinamonalisa.estudo@gmail.com>
2026-03-04 22:23:23 -0300

    Add files via upload

--- pages/cadastro/script.js
+++ pages/cadastro/script.js
@@ -1,23 +1,132 @@
-const inputName = document.getElementById("input-name");
-const inputEmail = document.getElementById("input-email");
-const inputCelular = document.getElementById("input-celular");
-const inputDataNascimento = document.getElementById("input-dataNascimento");
-const inputSenha = document.getElementById("input-senha");
-const inputConfirmeSenha = document.getElementById("input-confirmeSenha");
+const checkbox = document.getElementById("id-checkbox");
+const msgErrorSenha = document.getElementById("error-senha");
 
-function mostrarValorDoInput(){
-      const valorNome = inputName.value;
-      const valorEmail = inputEmail.value;
-      const valorCelular = inputCelular.value;
-      const valorDataNascimento = inputDataNascimento.value;
-      const valorSenha = inputSenha.value;
-      const valorConfirmeSenha = inputConfirmeSenha.value;
 
-      console.log("Nome:", valorNome );
-      console.log("E-mail:", valorEmail );
-      console.log("Celular:", valorCelular );
-      console.log("Data de Nascimento:", valorDataNascimento );
-      console.log("Senha:", valorSenha );
-      console.log("Confirme a senha:", valorConfirmeSenha );
+function validaCPF(cpf) {
+  var Soma = 0;
+  var Resto; //=null
+  //regex <¬
+  var strCPF = String(cpf).replace(/[^\d]/g, "");
+  //replace=subistibuir ->'123.456.789-00' -> 12345678900
+  if (strCPF.length !== 11) {
+    return false;
+  }
 
- }
+  if (
+    [
+      "00000000000",
+      "11111111111",
+      "22222222222",
+      "33333333333",
+      "44444444444",
+      "55555555555",
+      "66666666666",
+      "77777777777",
+      "88888888888",
+      "99999999999",
+    ].indexOf(strCPF) !== -1
+  )
+    return false;
+
+  for (i = 1; i <= 9; i++)
+    Soma = Soma + parseInt(strCPF.substring(i - 1, i)) * (11 - i);
+
+  Resto = (Soma * 10) % 11;
+
+  if (Resto == 10 || Resto == 11) Resto = 0;
+
+  if (Resto != parseInt(strCPF.substring(9, 10))) return false;
+
+  Soma = 0;
+
+  for (i = 1; i <= 10; i++)
+    Soma = Soma + parseInt(strCPF.substring(i - 1, i)) * (12 - i);
+
+  Resto = (Soma * 10) % 11;
+
+  if (Resto == 10 || Resto == 11) Resto = 0;
+
+  if (Resto != parseInt(strCPF.substring(10, 11))) return false;
+
+  return true;
+}
+
+function cadastrar(eventrec) {
+  event.preventDefault(eventrec);
+  const inputName = document.getElementById("input-name").value;
+  const inputCPF = document.getElementById("input-cpf").value;
+  const inputEmail = document.getElementById("input-email").value;
+  const inputCelular = document.getElementById("input-celular").value;
+  const inputDataNascimento = document.getElementById(
+    "input-dataNascimento",
+  ).value;
+  const inputSex = document.getElementById("input-sexo").value;
+  const inputSenha = document.getElementById("input-senha").value;
+  const inputConfirmeSenha = document.getElementById(
+    "input-confirmeSenha",
+  ).value;
+  const msgErrorName = document.getElementById("error-name");
+  const msgErrorCPF = document.getElementById("error-cpf");
+  const msgErrorEmail = document.getElementById("error-email");
+  const msgErrorCelular = document.getElementById("error-celular");
+  const msgErrorSexo = document.getElementById("error-sexo");
+  const msgErrorSenha = document.getElementById("error-senha");
+  const msgErrorConfirmeSenha = document.getElementById("error-confirmeSenha");
+
+  //0 || '' --> false => ! => true
+  if (!inputName || inputName.length < 3) {
+    msgErrorName.style.display = "block";
+  } else {
+    msgErrorName.style.display = "none";
+  }
+
+  if (!validaCPF(inputCPF)) {
+    msgErrorCPF.style.display = "block";
+  } else {
+    msgErrorCPF.style.display = "none";
+  }
+
+  if (!inputEmail || inputEmail.length < 6) {
+    msgErrorEmail.style.display = "block";
+  } else {
+    msgErrorEmail.style.display = "none";
+  }
+
+  if (!inputCelular || inputCelular.length < 10) {
+    msgErrorCelular.style.display = "block";
+  } else {
+    msgErrorCelular.style.display = "none";
+  }
+
+  if (!inputSex) {
+    msgErrorSexo.style.display = "block";
+  } else {
+    msgErrorSexo.style.display = "none";
+  }
+
+  console.log(!inputSenha || !(inputSenha.length > 8 && inputSenha.length < 100));
+  
+
+  if (!inputSenha || !(inputSenha.length >= 8 && inputSenha.length < 100)) {
+      
+      msgErrorSenha.style.display = "block";
+  } else {
+      msgErrorSenha.style.display = "none";
+  }
+
+  if (!inputConfirmeSenha || inputConfirmeSenha !== inputSenha) {
+      msgErrorConfirmeSenha.style.display = "block";
+  } else {
+      msgErrorConfirmeSenha.style.display = "none";
+  }
+}
+
+
+
+
+//endereço
+//   if(inputEndereco || inputEndereco < 6 ){
+//       alert ("Endereço inválido!")
+//   }
+
+// 0, "", null, undefined, false ----> false
--- pages/login/script.js
+++ pages/login/script.js
@@ -1,22 +1,56 @@
+const msgErrorSenha = document.getElementById("error-senha");
 const inputLogin = document.getElementById("input-login");
-//console.log (inputLogin);
-
 const inputSenha = document.getElementById("input-senha");
-//console.log(inputSenha);
 
+//FUNCTION
 function login(eventrec){
       event.preventDefault(eventrec)
-      const valorLogin = inputLogin.value;
-      const valorSenha = inputSenha.value;
+      const valorLogin = document.getElementById("input-login").value;
+      const valorSenha = document.getElementById("input-senha").value;
 
       const LOGIN_VALIDO = "admin@admin.com";
       const SENHA_VALIDA = '1234';
 
+
       if(valorLogin == LOGIN_VALIDO && valorSenha == SENHA_VALIDA){
-            window.location.href = "../listagem/index.html";
+           // window.location.href = "../listagem/index.html";
+            rememberLogin();
       }else{
-            alert("Login Inválido");
+            msgErrorSenha.style.display = "block";
+            inputLogin.style.border = "1px solid red"
+            inputSenha.style.border = "1px solid red"       
+  }
+  }
+
+  //ARROW FUNCTION
+  const checkboxRemember = document.querySelector('input[type=checkbox]');
+
+  const rememberLogin = () => {
+      const inputLoginRemember = document.getElementById("input-login").value;
+      const inputSenhaRemember = document.getElementById("input-senha").value;
+
+     //console.log(inputLoginRemember, inputSenhaRemember, checkboxRemember.checked);
+      
+      const dadosLogin = {
+            login: inputLoginRemember,
+            senha: inputSenhaRemember
       }
-}
+
+      if(checkboxRemember.checked){
+            localStorage.setItem('dadosLogin', JSON.stringify(dadosLogin))
+      }
+
+  }
+
+  const valorSalvo = JSON.parse(localStorage.getItem("dadosLogin"));
+  
+  if(valorSalvo.login && valorSalvo.senha){
+      inputLogin.value = valorSalvo.login;
+      inputSenha.value = valorSalvo.senha
+  }
+
+  
+
+
 
 
